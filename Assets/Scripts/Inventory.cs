a977dd7ac8053b0b89a87a64c3657634a10a16bd
Luiser-ttv <58303209+Luiser-ttv@users.noreply.github.com>
2023-02-04 00:19:26 +0100

    Items picked showing on the screen.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,6 +5,25 @@ public class Inventory : MonoBehaviour
 {
     public List<GameObject> items;
     public float pickupRadius = 2.0f;
+    public Vector2 itemDisplaySize = new Vector2(50, 50);
+    public int itemsPerColumn = 5;
+    private int currentColumn = 0;
+
+    void OnGUI()
+    {
+        int itemsDrawn = 0;
+        foreach (GameObject item in items)
+        {
+            Rect itemRect = new Rect(10 + currentColumn * itemDisplaySize.x, 10 + (itemsDrawn % itemsPerColumn) * itemDisplaySize.y, itemDisplaySize.x, itemDisplaySize.y);
+            GUI.Box(itemRect, item.GetComponent<SpriteRenderer>().sprite.texture);
+            itemsDrawn++;
+            if (itemsDrawn % itemsPerColumn == 0)
+            {
+                currentColumn++;
+            }
+        }
+        currentColumn = 0;
+    }
 
     void Update()
     {
